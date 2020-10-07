668b3dc7dd09c0c2e4b933d9ca9f8afd57e530db
Shawn Lim <shawnlim89@live.com>
2020-10-07 22:40:32 +0800

    Connect to github to commit

--- sidebar/src/App.svelte
+++ sidebar/src/App.svelte
@@ -1,15 +1,33 @@
 <script>
   export let name;
+  let githubLoginContainer;
 
-  const insertHelloWorld = () => {
-    google.script.run.insertText("Testing ADR logger");
+  const logAdr = () => {
+    google.script.run.commitSingleFileToGithub();
   }
+
+  function createElementFromHTML(htmlString) {
+    var div = document.createElement('div');
+    div.innerHTML = htmlString.trim();
+
+    // Change this to div.childNodes to support multiple top-level nodes
+    return div.firstChild; 
+  }
+
+  function onSuccess(urlHtml) {
+    const node = createElementFromHTML(urlHtml);
+    githubLoginContainer.appendChild(node);
+  }
+
+  google.script.run.withSuccessHandler(onSuccess).getGithubAuthURL()
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial" target="_blank">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-  <button on:click={insertHelloWorld} class="blue">Testing ADR logger.</button>
+  <p>Visit the <a href="https://svelte.dev/tutorial" target="_blank">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <strong>Open the following in a new tab:</strong>
+  <div bind:this={githubLoginContainer} />
+  <button on:click={logAdr} class="blue">Testing ADR logger.</button>
 </main>
 
 <style>
